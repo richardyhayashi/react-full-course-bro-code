459b9d3726b9ecb207ec05a199693f915ebde069
Richard Y. Hayashi <richardyhayashi@gmail.com>
2025-05-15 21:02:57 +0900

    section 3a.

--- 01/my-react-app/src/App.jsx
+++ 01/my-react-app/src/App.jsx
@@ -1,12 +1,9 @@
-import Card from "./components/Card";
+import Button from "./components/Button";
 
 function App() {
   return (
     <>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      <Button />
     </>
   );
 }
