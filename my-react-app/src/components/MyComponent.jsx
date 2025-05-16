75f8c9cae0145fd1895b8d15c1c1175a0d2caf43
Richard Y. Hayashi <richardyhayashi@gmail.com>
2025-05-16 21:13:49 +0900

    section16a.

--- my-react-app/src/components/MyComponent.jsx
+++ my-react-app/src/components/MyComponent.jsx
@@ -1,9 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const MyComponent = () => {
-   
+  const [count, setCount] = useState(0);
+  const [color, setColor] = useState('green');
+  
+    useEffect(() => {
+      document.title = `Count: ${count} ${color}`
+    }, [count, color]);
+
+    const addCount = () => {
+      setCount(c => c + 1);
+    }
+
+    const subtractCount = () => {
+      setCount((c) => c - 1);
+    };
+
+    const changeColor = () => {
+      setColor(c => c === 'green' ? 'red' : 'green');
+    }
+
     return (
-      <></>
+      <>
+        <p style={{color: color}}>Count: {count}</p>
+        <button onClick={addCount}>Add</button>
+        <button onClick={subtractCount}>Subtract</button><br />
+        <button onClick={changeColor}>Change Color</button>
+      </>
     );
 };
 
