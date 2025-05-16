e28296d4d88072883cfd363b4c59e949b71c3fa2
Richard Y. Hayashi <richardyhayashi@gmail.com>
2025-05-16 21:32:40 +0900

    Finished section16; 16b.

--- my-react-app/src/components/MyComponent.jsx
+++ my-react-app/src/components/MyComponent.jsx
@@ -1,33 +1,34 @@
 import React, { useEffect, useState } from "react";
 
 const MyComponent = () => {
-  const [count, setCount] = useState(0);
-  const [color, setColor] = useState('green');
+  const [width, setWidth] = useState(window.innerWidth);
+  const [height, setHeight] = useState(window.innerHeight);
   
-    useEffect(() => {
-      document.title = `Count: ${count} ${color}`
-    }, [count, color]);
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    console.log("EVENT LISTENER ADDED");
 
-    const addCount = () => {
-      setCount(c => c + 1);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+      console.log("EVENT LISTENER REMOVED");
     }
+  }, []);
 
-    const subtractCount = () => {
-      setCount((c) => c - 1);
-    };
+  useEffect(() => {
+    document.title = `Size: ${width} x ${height}`
+  }, [width, height]);
 
-    const changeColor = () => {
-      setColor(c => c === 'green' ? 'red' : 'green');
-    }
+  const handleResize = () => {
+    setWidth(window.innerWidth);
+    setHeight(window.innerHeight);
+  }
 
-    return (
-      <>
-        <p style={{color: color}}>Count: {count}</p>
-        <button onClick={addCount}>Add</button>
-        <button onClick={subtractCount}>Subtract</button><br />
-        <button onClick={changeColor}>Change Color</button>
-      </>
-    );
+  return (
+    <>
+      <p>Window Width: {width}px</p>
+      <p>Window Height: {height}px</p>
+    </>
+  );
 };
 
 export default MyComponent;
