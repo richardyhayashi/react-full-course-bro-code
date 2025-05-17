add344f5d72ef3f00411b1a0ed6b5918597a7c9b
Richard Y. Hayashi <richardyhayashi@gmail.com>
2025-05-17 19:21:27 +0900

    Finished section19;19b.

--- my-react-app/src/components/MyComponent.jsx
+++ my-react-app/src/components/MyComponent.jsx
@@ -1,20 +1,46 @@
 import React, {useEffect, useRef, useState } from 'react';
 
 const MyComponent = () => {
-   let [number, setNumber] = useState(0);
+   const inputRef1 = useRef(null);
+   const inputRef2 = useRef(null);
+   const inputRef3 = useRef(null);
 
    useEffect(() => {
       console.log("COMPONENT RENDERED");
    });
 
-   const handleClick = () => {
-      setNumber(n => n + 1);
+   const handleClick1 = () => {
+      inputRef1.current.focus();
+      inputRef1.current.style.backgroundColor = "yellow";
+      inputRef2.current.style.backgroundColor = '';
+      inputRef3.current.style.backgroundColor = '';
    }
 
+   const handleClick2 = () => {
+     inputRef2.current.focus();
+     inputRef1.current.style.backgroundColor = '';
+     inputRef2.current.style.backgroundColor = 'yellow';
+     inputRef3.current.style.backgroundColor = '';
+   };
+
+   const handleClick3 = () => {
+     inputRef3.current.focus();
+     inputRef1.current.style.backgroundColor = '';
+     inputRef2.current.style.backgroundColor = '';
+     inputRef3.current.style.backgroundColor = 'yellow';
+   };
+
    return (
-      <button onClick={handleClick}>
-         Click me!
-      </button>
+     <div>
+       <button onClick={handleClick1}>Click me1!</button>
+       <input ref={inputRef1} />
+
+       <button onClick={handleClick2}>Click me2!</button>
+       <input ref={inputRef2} />
+
+       <button onClick={handleClick3}>Click me3!</button>
+       <input ref={inputRef3} />
+     </div>
    );
 };
 
